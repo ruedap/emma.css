734d348991d4e6fa350fe6606fe53ecee2b9c2b6
ruedap <ruedap@ruedap.com>
2017-06-20 14:41:49 +0900

    Format .ts files using Prettier
    `prettier --write src/*.ts test/*.ts`

--- test/emma-data.test.ts
+++ test/emma-data.test.ts
@@ -7,88 +7,92 @@ import * as fs from "fs-extra";
 import * as _ from "lodash";
 import * as semverRegex from "semver-regex";
 
-describe("emma-data.json",() => {
+describe("emma-data.json", () => {
   let emma;
   let doc;
 
   beforeEach(() => {
     emma = new Emma();
-    doc = JSON.parse(fs.readFileSync(emma.EMMA_JSON, 'utf8'));
+    doc = JSON.parse(fs.readFileSync(emma.EMMA_JSON, "utf8"));
   });
 
   it("is valid length", () => {
     assert(doc.vars.length === 55);
     assert(doc.rules.mixins.length === 32);
     assert(doc.rules.props.length === 106);
-    const propValuesLength = _.reduce(doc.rules.props, (result, v) => {
-      return result + v.values.length;
-    }, 0);
+    const propValuesLength = _.reduce(
+      doc.rules.props,
+      (result, v) => {
+        return result + v.values.length;
+      },
+      0
+    );
     assert(propValuesLength === 714);
   });
 
   it("is unique abbreviations", () => {
     let actual = [];
-    _.each(doc.rules.props, (p) => {
-      _.each(p.values, (v) => {
+    _.each(doc.rules.props, p => {
+      _.each(p.values, v => {
         actual.push(emma.generateAbbr(p.abbr, v.abbr));
       });
     });
-    _.each(doc.rules.mixins, (m) => {
+    _.each(doc.rules.mixins, m => {
       actual.push(m.abbr);
     });
     assert(_.isEqual(actual, _.uniq(actual)));
   });
 
-  describe("[ver]",() => {
+  describe("[ver]", () => {
     it("is valid SemVer", () => {
       assert(semverRegex().test(doc.ver));
     });
   });
 
-  describe("[vars]",() => {
+  describe("[vars]", () => {
     it("is unique [name]", () => {
-      let actual = _.map(doc.vars, 'name');
+      let actual = _.map(doc.vars, "name");
       assert(_.isEqual(actual, _.uniq(actual)));
     });
   });
 
-  describe("[rules]",() => {
-    describe("[props]",() => {
+  describe("[rules]", () => {
+    describe("[props]", () => {
       it("is unique [name]", () => {
-        let actual = _.map(doc.rules.props, 'name');
+        let actual = _.map(doc.rules.props, "name");
         assert(_.isEqual(actual, _.uniq(actual)));
       });
 
       it("is unique [abbr]", () => {
-        let actual = _.map(doc.rules.props, 'abbr');
+        let actual = _.map(doc.rules.props, "abbr");
         assert(_.isEqual(actual, _.uniq(actual)));
       });
 
-      describe("[values]",() => {
+      describe("[values]", () => {
         it("is unique [name]", () => {
-          _.forEach(doc.rules.props, (p) => {
-            let actual = _.map(p.values, 'name');
+          _.forEach(doc.rules.props, p => {
+            let actual = _.map(p.values, "name");
             assert(_.isEqual(actual, _.uniq(actual)));
           });
         });
 
         it("is unique [abbr]", () => {
-          _.forEach(doc.rules.props, (p) => {
-            let actual = _.map(p.values, 'abbr');
+          _.forEach(doc.rules.props, p => {
+            let actual = _.map(p.values, "abbr");
             assert(_.isEqual(actual, _.uniq(actual)));
           });
         });
       });
     });
 
-    describe("[mixins]",() => {
+    describe("[mixins]", () => {
       it("is unique [name]", () => {
-        let actual = _.map(doc.rules.mixins, 'name');
+        let actual = _.map(doc.rules.mixins, "name");
         assert(_.isEqual(actual, _.uniq(actual)));
       });
 
       it("is unique [abbr]", () => {
-        let actual = _.map(doc.rules.mixins, 'abbr');
+        let actual = _.map(doc.rules.mixins, "abbr");
         assert(_.isEqual(actual, _.uniq(actual)));
       });
     });
--- test/emmet.test.ts
+++ test/emmet.test.ts
@@ -3,7 +3,7 @@ import * as sinon from "sinon";
 import * as assert from "power-assert";
 
 import * as parse from "@emmetio/css-abbreviation";
-import * as SnippetsRegistry  from "@emmetio/snippets-registry";
+import * as SnippetsRegistry from "@emmetio/snippets-registry";
 import * as Profile from "@emmetio/output-profile";
 import resolveSnippets from "@emmetio/css-snippets-resolver";
 import stringify from "@emmetio/stylesheet-formatters";
@@ -12,72 +12,76 @@ import Emma from "../src/emma";
 import * as fs from "fs-extra";
 import * as _ from "lodash";
 
-describe("emma-data.json",() => {
+describe("emma-data.json", () => {
   let emma;
   let doc;
   let expand;
 
   before(() => {
     emma = new Emma();
-    doc = JSON.parse(fs.readFileSync(emma.EMMA_JSON, 'utf8'));
+    doc = JSON.parse(fs.readFileSync(emma.EMMA_JSON, "utf8"));
     const registry = new SnippetsRegistry();
     const emmaProps = {};
-    _.each(doc.rules.props, (p) => {
-      let values = '';
-      _.each(_.reverse(p.values), (v) => {
-        values += `${v.name}|`
+    _.each(doc.rules.props, p => {
+      let values = "";
+      _.each(_.reverse(p.values), v => {
+        values += `${v.name}|`;
       });
-      values = _.trimEnd(values, '|');
+      values = _.trimEnd(values, "|");
       // TODO: Fix values (e.g. font-family)
       // console.log(values);
-      _.assign(emmaProps, {[p.abbr]: `${p.name}:${values}`});
+      _.assign(emmaProps, { [p.abbr]: `${p.name}:${values}` });
     });
     registry.add(emmaProps);
 
     expand = (abbr, profile, syntax, options) => {
       const tree = resolveSnippets(parse(abbr), registry);
       return stringify(tree, profile || new Profile(), syntax, options);
-    }
+    };
   });
 
-  describe("[rules] [props]",() => {
+  describe("[rules] [props]", () => {
     it("is valid Emmet abbreviation spec", () => {
-      _.each(doc.rules.props, (p) => {
-        _.each(p.values, (v) => {
+      _.each(doc.rules.props, p => {
+        _.each(p.values, v => {
           // FIXME: variable name
-          if (emma.isVar(v.name)) { return; }
+          if (emma.isVar(v.name)) {
+            return;
+          }
           const abbr = emma.generateAbbr(p.abbr, v.abbr);
           // FIXME: Invalid Emmet abbreviation list
           const invalidEmmetAbbrList = [
-            'm-a',
-            'm0-a',
-            'm-a-0',
-            'ff-a',
-            'ff-t',
-            'ff-v',
-            'ff-l',
-            'ff-ja',
-            'op0_1',
-            'op0_2',
-            'op0_3',
-            'op0_4',
-            'op0_5',
-            'op0_6',
-            'op0_7',
-            'op0_8',
-            'op0_9',
-            'fxw-wr',
-            'ord1',
-            'ord2',
-            'ord3',
-            'ord4',
-            'ord5',
-            'ord6',
-            'ord9999',
-            'ord-1',
-            'fx1-0-0',
+            "m-a",
+            "m0-a",
+            "m-a-0",
+            "ff-a",
+            "ff-t",
+            "ff-v",
+            "ff-l",
+            "ff-ja",
+            "op0_1",
+            "op0_2",
+            "op0_3",
+            "op0_4",
+            "op0_5",
+            "op0_6",
+            "op0_7",
+            "op0_8",
+            "op0_9",
+            "fxw-wr",
+            "ord1",
+            "ord2",
+            "ord3",
+            "ord4",
+            "ord5",
+            "ord6",
+            "ord9999",
+            "ord-1",
+            "fx1-0-0"
           ];
-          if (_.includes(invalidEmmetAbbrList, abbr)) { return; }
+          if (_.includes(invalidEmmetAbbrList, abbr)) {
+            return;
+          }
           assert.equal(expand(abbr), `${p.name}: ${v.name};`);
         });
       });
