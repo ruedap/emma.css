7fb6b5f74be31a1c4cf8df2d8774fa5b237bee95
ruedap <ruedap@ruedap.com>
2018-03-29 01:34:14 +0900

    build(npm): Add bump command

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,8 +1,32 @@
-import gulp from 'gulp';
-import removeEmptyLines from 'gulp-remove-empty-lines';
+import gulp from "gulp";
+import removeEmptyLines from "gulp-remove-empty-lines";
+import replace from "gulp-replace";
+import yaml from "js-yaml";
+import minimist from "minimist";
+import fs from "fs";
 
-gulp.task('remove-empty-lines', () => {
-  gulp.src('./emma.css')
-  .pipe(removeEmptyLines())
-  .pipe(gulp.dest('./'));
+gulp.task("remove-empty-lines", () => {
+  gulp.src("./emma.css").pipe(removeEmptyLines()).pipe(gulp.dest("./"));
+});
+
+gulp.task("bump-version-number", () => {
+  const ymlPath = "./src/emma-data.yml";
+  const bowerPath = "./bower.json";
+  const doc = yaml.safeLoad(fs.readFileSync(ymlPath, "utf8"));
+  const argv = minimist(process.argv.slice(2));
+
+  if (argv.v === true) {
+    throw new Error("Invalid arguments");
+  }
+
+  gulp
+    .src(ymlPath)
+    .pipe(replace(`ver: '${doc.ver}'`, `ver: '${argv.v}'`))
+    .pipe(gulp.dest("./src"));
+  gulp
+    .src(bowerPath)
+    .pipe(replace(`"version": "${doc.ver}"`, `"version": "${argv.v}"`))
+    .pipe(gulp.dest("./"));
+
+  console.log(`Bumped version number: ${doc.ver} -> ${argv.v}`);
 });
